fdbb0bfe292470a34fa6e5e57c18178248353628
Joe <joeschwehr@gmail.com>
2019-10-23 15:07:16 -0700

    working on face recognition

--- controllers/image.js
+++ controllers/image.js
@@ -9,7 +9,6 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.inputField)
     .then(data => {
-        console.log("data: ", data);
         res.json(data);
     })
     .catch(err => res.status(400).json("unable to work with API"))
