49e319b5ba00559c57be55ea430cdd9c11b8fefd
Joe <joeschwehr@gmail.com>
2019-10-23 11:53:19 -0700

    removed all semicolons from server.js... prepare for all hell to break loose

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const express = require("express")
+const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt')
+const cors = require('cors')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 const PORT = process.env.PORT || 3000
 
-const knex = require('knex');
+const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
@@ -17,31 +17,31 @@ const db = knex({
         password : '',
         database : 'smartbrain'
     }
-});
+})
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
 // homepage
-app.get("/", (req, res) => {res.send("working")});
+app.get("/", (req, res) => {res.send("Joe's app is up and running! Today! 528491")})
 
 // signin: POST = success fail
-app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt))
 
 // register a new user/add new user to db
 // register: POST returns user
-app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt));
+app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt))
 
 // profile/:userId: GET = user
-app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db))
 
 // image: PUT returns user with updated entries
-app.put("/image", (req, res) => image.handleImage(req, res, db));
+app.put("/image", (req, res) => image.handleImage(req, res, db))
 
-app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res))
 
 app.listen(PORT, () => {
-    console.log("Listening on port", PORT);
-});
+    console.log("Listening on port", PORT)
+})
