9f8a4fc006c4ae52ac716e28c05afc98cedafe9d
Joe <joeschwehr@gmail.com>
2019-10-22 20:19:49 -0700

    server.js update

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
 const port = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on port", port);
+    console.log("Listening on port", process.env.PORT);
 });
