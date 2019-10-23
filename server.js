76f32af47991e1ce9db2768b624facfdb040a9fc
Joe <joeschwehr@gmail.com>
2019-10-23 11:34:39 -0700

    updated server and added node engine in package.json

--- server.js
+++ server.js
@@ -6,42 +6,42 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const PORT = process.env.PORT || 3000
 
-// const knex = require('knex');
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//         host : '127.0.0.1',
-//         user : 'macairjps',
-//         password : '',
-//         database : 'smartbrain'
-//     }
-// });
+const knex = require('knex');
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'macairjps',
+        password : '',
+        database : 'smartbrain'
+    }
+});
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 // homepage
-app.get("/", (req, res) => {res.send("working... hoo rah! October 21, 2015. JPS.")});
+app.get("/", (req, res) => {res.send("working")});
 
-// // signin: POST = success fail
-// app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+// signin: POST = success fail
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
-// // register a new user/add new user to db
-// // register: POST returns user
-// app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt));
+// register a new user/add new user to db
+// register: POST returns user
+app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt));
 
-// // profile/:userId: GET = user
-// app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
+// profile/:userId: GET = user
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
 
-// // image: PUT returns user with updated entries
-// app.put("/image", (req, res) => image.handleImage(req, res, db));
+// image: PUT returns user with updated entries
+app.put("/image", (req, res) => image.handleImage(req, res, db));
 
-// app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port", PORT);
 });
